8f3a6c069a2e6712de26e56b90ce53b98b51c4ca
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2018-12-17 16:29:35 -0500

    Creating schematic page

---
+++ src/components/schematic/SchematicViewer.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+
+class SchematicViewer extends Component {
+
+    constructor(props) {
+        super(props);
+
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Schematic Viewer Page</h1>
+            </div>
+        );
+    }
+}
+
+export default SchematicViewer;
