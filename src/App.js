81e31fdea1876788baa3db8d22b84c45187b54d7
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2018-12-17 17:14:47 -0500

    Implemented minimal dataviz page and react router to navigate to it

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
 import React, { Component } from 'react';
+import { Container } from 'reactstrap';
+import { HashRouter, Switch, Route } from 'react-router-dom';
+
 import SchematicViewer from './components/schematic/SchematicViewer';
+import DataVizViewer from './components/dataviz/DataVizViewer';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <SchematicViewer />
-      </div>
+        <Container fluid>
+          <HashRouter>
+            <Switch>
+              <Route exact path="/" component={SchematicViewer} />
+              <Route path="/data" component={DataVizViewer} />
+            </Switch>
+          </HashRouter>
+        </Container>
     );
   }
 }
