0a64af57e457fa8e23353430449997a5ca439e51
Ross Smith <rossmith@Rosss-MacBook-Pro.local>
2018-12-19 14:17:07 -0500

    KPMP-549 schematic JSON implemented with simple links into data page by cell tpye

--- src/components/schematic/SchematicViewer.js
+++ src/components/schematic/SchematicViewer.js
@@ -1,16 +1,31 @@
 import React, { Component } from 'react';
+import schematic from '../../data/schematic';
+import { Col, Row, ListGroup, ListGroupItem } from 'reactstrap';
+import { Link } from 'react-router-dom';
 
 class SchematicViewer extends Component {
 
     constructor(props) {
         super(props);
-
     }
 
     render() {
         return (
             <div>
                 <h1>Schematic Viewer Page</h1>
+                <Col>
+                    { Object.values(schematic.schematic).map((structure, i) => {
+
+                        return <ListGroup>
+                            {structure.map((cellName, j) => {
+                                let href="/data/" + encodeURIComponent(cellName);
+                                return <ListGroupItem>
+                                           <Link to={href}>{cellName}</Link>
+                                       </ListGroupItem>
+                            })}
+                        </ListGroup>
+                    })}
+                </Col>
             </div>
         );
     }
